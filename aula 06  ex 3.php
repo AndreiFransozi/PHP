94865903dcf4db7ca299fabb95d2e649a9ed7253
AndreiFransozi <66423983+AndreiFransozi@users.noreply.github.com>
2022-03-16 13:11:48 -0300

    Add files via upload

---
+++ aula 06  ex 3.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>ex 03</title>
+ </head>
+ <body>
+ <?php 
+	$a = 3;
+	$b = $a;
+	$b += 5;
+	echo "A variavel a vale $a";
+	echo "<br/>A variavel B vale $b";
+ ?>
+ </body>
+</html>
---
+++ aula06d.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>ex 04</title>
+ </head>
+ <body>
+ <?php 
+	$x = "abc";
+	$$x = "def";
+	echo "O conteudo da variavel X e $x";
+	echo "<br>A variavel criada recebeu o valor $abc"
+ ?>
+ </body>
+</html>
